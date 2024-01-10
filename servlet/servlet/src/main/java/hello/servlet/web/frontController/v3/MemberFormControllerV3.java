7c8b98b4f3920c0bea59babc42a5714d90d8ddc6
김병웅 <quddnddl35@naver.com>
2024-01-10 16:39:37 +0900

    0110

---
+++ servlet/servlet/src/main/java/hello/servlet/web/frontController/v3/MemberFormControllerV3.java
@@ -0,0 +1,13 @@
+package hello.servlet.web.frontController.v3;
+
+import hello.servlet.web.frontController.ModelView;
+
+import java.util.Map;
+
+public class MemberFormControllerV3 implements ControllerV3{
+
+  @Override
+  public ModelView process(Map<String, String> paramMap) {
+    return new ModelView("new-form");
+  }
+}
