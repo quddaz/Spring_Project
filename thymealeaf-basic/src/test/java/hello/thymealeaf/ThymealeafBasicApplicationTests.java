d99f595446e8d01484f733bb26b92bb44448ca52
김병웅 <quddnddl35@naver.com>
2024-01-21 18:18:30 +0900

    0121

---
+++ thymealeaf-basic/src/test/java/hello/thymealeaf/ThymealeafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymealeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymealeafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
