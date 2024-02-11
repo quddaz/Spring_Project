2aa38cc05e685257c274f13bea8a7d8e08e637b2
김병웅 <quddnddl35@naver.com>
2024-02-11 15:38:01 +0900

    파일 업로드 완 0211

---
+++ 김영한님의 스프링 MVC 2편/upload/src/main/java/hello/upload/domain/UploadFile.java
@@ -0,0 +1,15 @@
+package hello.upload.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class UploadFile {
+  private String uploadFileName;
+  private String storeFileName;
+  @Builder
+  public UploadFile(String uploadFileName, String storeFileName) {
+    this.uploadFileName = uploadFileName;
+    this.storeFileName = storeFileName;
+  }
+}
