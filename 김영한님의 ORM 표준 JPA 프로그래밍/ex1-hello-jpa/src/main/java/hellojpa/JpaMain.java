5fd1bdb719a07fef19de50d686a95a4968eda359
김병웅 <quddnddl35@naver.com>
2024-02-13 16:31:25 +0900

    jpa 0213

--- 김영한님의 ORM 표준 JPA 프로그래밍/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김영한님의 ORM 표준 JPA 프로그래밍/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import hellojpa.domain.Member;
 import jakarta.persistence.*;
 
 public class JpaMain {
@@ -9,8 +10,57 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        /*
+        //저장
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("helloA");
+            em.persist(member);
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        //조회
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id = "+ findMember.getId());
+            System.out.println("findMember.name =" + findMember.getName());
 
-        em.close();
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        //삭제
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            em.remove(findMember);
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+         */
+        //수정
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJpa");
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
         emf.close();
     }
 }
