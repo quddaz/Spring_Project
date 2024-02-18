3e03c12360c7716d85f85b16dd1051d82d4052cb
김병웅 <quddnddl35@naver.com>
2024-02-18 15:41:40 +0900

    고급 매핑 0218

---
+++ 김영한님의 ORM 표준 JPA 프로그래밍/ex1-hello-jpa/src/main/java/hellojpa/domain/Locker.java
@@ -0,0 +1,15 @@
+package hellojpa.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Locker {
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String name;
+}
