f5c28eed7927dcb82c256908f1d34ed0ef53f95d
김병웅 <quddnddl35@naver.com>
2024-02-25 17:27:36 +0900

    페치조인 0225

--- 김영한님의 ORM 표준 JPA 프로그래밍/jpql/src/main/java/hellojpa/JpaMain.java
+++ 김영한님의 ORM 표준 JPA 프로그래밍/jpql/src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,11 @@
 package hellojpa;
 
 import hellojpa.domain.Member;
+import hellojpa.domain.Team;
 import hellojpa.domain.ValueType.Address;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 public class JpaMain {
 
@@ -14,12 +17,30 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        //수정
         try {
+            Team team = new Team();
+            em.persist(team);
 
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(team);
+            em.persist(member1);
 
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(team);
+            em.persist(member2);
 
-
+            em.flush();
+            em.clear();
+            String jpql = "select m from Member m join fetch m.team";
+            List<Member> members = em.createQuery(jpql, Member.class)
+                .getResultList();
+            for (Member member : members) {
+                //페치 조인으로 회원과 팀을 함께 조회해서 지연 로딩X
+                System.out.println("username = " + member.getUsername() + ", " +
+                    "teamName = " + member.getTeam().getName());
+            }
             tx.commit();
         }catch (Exception e){
             tx.rollback();
