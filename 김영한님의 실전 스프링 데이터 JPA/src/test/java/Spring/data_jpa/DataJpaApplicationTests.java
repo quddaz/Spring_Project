d28fb77f275c8612c61e40dd62fcae10c2508ade
김병웅 <quddnddl35@naver.com>
2024-07-19 18:23:43 +0900

    init: 스프링 데이터 JPA 시작

---
+++ 김영한님의 실전 스프링 데이터 JPA/src/test/java/Spring/data_jpa/DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
